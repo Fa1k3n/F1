73aa3c3832324e60f126dc52222177d86174bbc3
Falken <johan.falkenstrom@gmail.com>
2015-05-04 20:44:41 +0200

    Initial commit

---
+++ example.asm
@@ -0,0 +1,10 @@
+; Comment starts with semicolon
+
+; A compiler directive starts with a .
+.alias OUTPUT R0
+.start 0x10
+
+; A label has text followed by colon
+; labels can contain all alpha chars, big and small
+a_label:
+
