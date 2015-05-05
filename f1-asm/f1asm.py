e59e64f90af021c02bf62b902ee1d8e57506ec20
ejohfal <johan.falkenstrom@ericsson.com>
2015-05-05 15:13:27 +0200

    skeleton app

--- f1-asm/f1asm.py
+++ f1-asm/f1asm.py
@@ -1,4 +1,52 @@
 
 class assembler:
+    op_codes = {
+        "mv_imm": 2,
+        "mv_reg": 3
+    }
+
+    def tokenize(self, line):
+        line = line.replace(",", "")
+        return line.split()
+
+    def decode_imm(self, imm_str):
+        if imm_str[0] != "#":
+            raise Exception
+
+        return int(imm_str[1:])
+
+    def decode_reg(self, reg_str):
+        return int(reg_str[1:])
+
+    def decode_mnemonic(self, mnemonic):
+        return assembler.op_codes[mnemonic]
+
+    def is_comment(self, line):
+        return line[0] == ";"
+
+    def is_compiler_directive(self, line):
+        return line[0] == "."
+
+    def prepare_line(self, line):
+        line = line.lower()
+        return line.lstrip()
+
     def decode_line(self, line):
-        pass
+        line = self.prepare_line(line)
+        if self.is_comment(line):
+            return None
+        if self.is_compiler_directive(line):
+            return None
+        (mnemonic, arg1, arg2) = self.tokenize(line)
+        if mnemonic == "mv":
+            try:
+                arg1 = self.decode_imm(arg1)
+                mnemonic += "_imm"
+            except Exception:
+                arg1 = self.decode_reg(arg1)
+                mnemonic += "_reg"
+            arg2 = self.decode_reg(arg2)
+        op_code = self.decode_mnemonic(mnemonic)
+
+        return [op_code, arg1, arg2]
+
--- f1-asm/f1asmTest.py
+++ f1-asm/f1asmTest.py
@@ -5,8 +5,24 @@ from f1asm import *
 class TestF1Asm(unittest.TestCase):
     def testMvImm(self):
         asm = assembler()
-        asm.decode_line("mv #2, r2")
+        out = asm.decode_line("mv #2, r2")
+        self.assertEqual(out, [2, 2, 2])
+        out = asm.decode_line("mv r0, r1")
+        self.assertEqual(out, [3, 0, 1])
+        out = asm.decode_line("MV r0, R1")
+        self.assertEqual(out, [3, 0, 1])
 
+    def testComment(self):
+        asm = assembler()
+        out = asm.decode_line("; This is a comment and should be skipped")
+        self.assertIsNone(out)
+        out = asm.decode_line("      ; Leading whitespaces should be trimmed")
+        self.assertIsNone(out)
+
+    def testCompilerDirective(self):
+        asm = assembler()
+        out = asm.decode_line(".start $1a")
+        self.assertIsNone(out)
 
 if __name__ == '__main__':
     unittest.main()
